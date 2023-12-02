3c0c00206b1b62eef184d393dca24d86b308d135
Ian Goh <ian.goh@jhu.edu>
2023-12-02 15:35:37 -0500

    day01
    make digit validation a function

--- day01/day01 - part2.py
+++ day01/day01 - part2.py
@@ -19,6 +19,43 @@ file.close()
 sum_calibration_values = 0
 line_count = 0
 
+def isValidDigit(c_pos,line):
+    c = line[c_pos] # character at that position
+
+    if (c >= '0' and c <= '9'):
+        # is a number so return it
+        return int(c)
+
+    if (c == 'o') and (line[c_pos+1] == 'n') and (line[c_pos+2] == 'e'):
+         return 1
+         
+    if (c == 't') and (line[c_pos+1] == 'w') and (line[c_pos+2] == 'o'):
+         return 2
+
+    if (c == 't') and (line[c_pos+1] == 'h') and (line[c_pos+2] == 'r') and (line[c_pos+3] == 'e') and (line[c_pos+4] == 'e'):
+         return 3
+
+    if (c == 'f') and (line[c_pos+1] == 'o') and (line[c_pos+2] == 'u') and (line[c_pos+3] == 'r'):
+         return 4
+
+    if (c == 'f') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'v') and (line[c_pos+3] == 'e'):
+         return 5
+
+    if (c == 's') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'x'):
+         return 6
+
+    if (c == 's') and (line[c_pos+1] == 'e') and (line[c_pos+2] == 'v') and (line[c_pos+3] == 'e') and (line[c_pos+4] == 'n'):
+         return 7
+
+    if (c == 'e') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'g') and (line[c_pos+3] == 'h') and (line[c_pos+4] == 't'):
+         return 8
+
+    if (c == 'n') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'n') and (line[c_pos+3] == 'e'):
+         return 9
+
+    # not a valid digit
+    return -1
+
 # process list
 for line in list:
 
@@ -29,47 +66,15 @@ for line in list:
     lastDigit = -1
     value = 0
 
-    c_pos = 0
-    for c in line:
+    for c_pos in range(0, len(line)):
+        # check if it's a valid digit
         validDigit = -1
-        
-        if (c >= '0' and c <= '9'):
-            # print (c, "is num")
-            validDigit = int(c)
-
-        if (validDigit == -1) and (c == 'o') and (line[c_pos+1] == 'n') and (line[c_pos+2] == 'e'):
-             validDigit = 1
-             
-        if (validDigit == -1) and (c == 't') and (line[c_pos+1] == 'w') and (line[c_pos+2] == 'o'):
-             validDigit = 2
-
-        if (validDigit == -1) and (c == 't') and (line[c_pos+1] == 'h') and (line[c_pos+2] == 'r') and (line[c_pos+3] == 'e') and (line[c_pos+4] == 'e'):
-             validDigit = 3
-
-        if (validDigit == -1) and (c == 'f') and (line[c_pos+1] == 'o') and (line[c_pos+2] == 'u') and (line[c_pos+3] == 'r'):
-             validDigit = 4
-
-        if (validDigit == -1) and (c == 'f') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'v') and (line[c_pos+3] == 'e'):
-             validDigit = 5
-
-        if (validDigit == -1) and (c == 's') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'x'):
-             validDigit = 6
-
-        if (validDigit == -1) and (c == 's') and (line[c_pos+1] == 'e') and (line[c_pos+2] == 'v') and (line[c_pos+3] == 'e') and (line[c_pos+4] == 'n'):
-             validDigit = 7
-
-        if (validDigit == -1) and (c == 'e') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'g') and (line[c_pos+3] == 'h') and (line[c_pos+4] == 't'):
-             validDigit = 8
-
-        if (validDigit == -1) and (c == 'n') and (line[c_pos+1] == 'i') and (line[c_pos+2] == 'n') and (line[c_pos+3] == 'e'):
-             validDigit = 9
-             
+        validDigit = isValidDigit(c_pos, line)
+       
         if validDigit != -1:
             if (firstDigit == -1):
                 firstDigit = validDigit
             lastDigit = validDigit
-            
-        c_pos += 1
         
     # print (firstDigit,lastDigit)
     calibration_value = 10 * firstDigit + lastDigit
